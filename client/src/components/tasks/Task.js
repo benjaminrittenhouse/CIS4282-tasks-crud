6308da053ef83b36977028d5f31bd1cccb14b9c8
Benjamin Rittenhouse <benjamin.rittenhouse@temple.edu>
2023-02-15 21:46:59 -0500

    setting up tasks display + insert

--- client/src/components/tasks/Task.js
+++ client/src/components/tasks/Task.js
@@ -1,29 +1,22 @@
 
-/*import { Link } from "react-router-dom"
+import { Link } from "react-router-dom"
 import { useState, useEffect } from "react";
-import UpdateModal from "./webUser/updateModal"
-import "../style/update.css"
+import "../../style/update.css"
 
 function Task({ props }) {
-
-  const [show, setShow] = useState(false)
-  const [modalVisibility, setModalVisibility] = useState("updateHide");
-
-  function handleShow() {
-    setShow(!show);
-  }
-
 
   useEffect(() => {
   });
 
-  const userObj =
+  const taskObj =
   {
     "taskID": props.task_id,
     "taskName": props.task_name,
     "taskDesc": props.task_desc,
     "taskPoints": props.task_points,
-    "taskAssigned": props.task_assigned
+    "targetDate": props.target_date,
+    "completionDate": props.completion_date,
+    "assignedName": props.full_name
   }
 
   return (
@@ -33,28 +26,23 @@ function Task({ props }) {
 
             <div className="expanded-grid-container">
 
-              <div className="expanded-grid-child-image">
-                <img className="imageThumb" src={props.image} alt="not found" />
-              </div>
 
               <div className="expanded-grid-child-info">
-                <h2><u>User {props.web_user_id}</u></h2>
-                <p>Email: {props.user_email}</p>
-                <p>Password: {props.user_password}</p>
-                <p>Membership Fee: {props.membership_fee}</p>
-                <p>Room #: {props.room_number}</p>
-                <p>Birthday: {props.birthday}</p>
-                <h4>Role: {props.role_type}</h4>
+                <h2><u>Task #{props.task_id}</u></h2>
+                <p>Name: {props.task_name}</p>
+                <p>Description: {props.task_desc}</p>
+                <p>Points: {props.task_points}</p>
+                <p>Target Date: {props.target_date}</p>
+                <p>Completion Date: {props.completion_date}</p>
+                <h4>Assigned: {props.full_name}</h4>
               </div>
 
-              <div className="expanded-grid-child-buttons">
-                <button onClick={handleShow}>Collapse</button>
-                <button onClick={handleUpdate} className="editButton">Edit</button>
-              </div>
+              
             </div>
 
           </div>
           </div>
         )
+  }
 
-export default Task;*/
+export default Task;
--- server/taskAPI/readAPI.js
+++ server/taskAPI/readAPI.js
@@ -12,7 +12,7 @@ router.use(express.urlencoded({ extended: false }));
 // list all tasks
 router.get("/listAllTasks", (req, res) => {
     try {
-      const sqlGet = "SELECT task_id, task_name, task_desc, task_points, assigned_web_user_id , user_email FROM db.tasks, db.web_user "
+      const sqlGet = "SELECT task_id, task_name, task_desc, task_points, target_date, completion_date, assigned_web_user_id, user_email FROM db.tasks, db.web_user "
       + "WHERE tasks.assigned_web_user_id = web_user.web_user_id";
       db.query(sqlGet, (req, result) => {
           res.send(result);
