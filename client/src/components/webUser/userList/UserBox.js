cc2605642070842e7c5d8e09632629c5ccb02703
Benjamin Rittenhouse <benjamin.rittenhouse@temple.edu>
2023-03-11 10:30:07 -0500

    delete working

--- client/src/components/webUser/userList/UserBox.js
+++ client/src/components/webUser/userList/UserBox.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Avatar, Box, IconButton, Typography, Button } from '@mui/material';
 import { Edit, Delete } from '@mui/icons-material';
 import DbToObj from '../DbToObj';
 import './UserBox.css';
+
+import ConfirmDeleteModal from '../delete/ConfirmDeleteModal';
 
 
 function UserBox({ userData, setExpandedUser, setIsEditing, processDelete, index}) {
@@ -18,14 +20,27 @@ function UserBox({ userData, setExpandedUser, setIsEditing, processDelete, index
     await processDelete(userObj.webUserId, index);
   }
 
+  function handleClickDelete(){
+     setIsDeleteModal(true)
+  }
+
   function handleClick() {
       console.log("user:")
       console.dir(userObj)
       setExpandedUser(userObj)
   }
 
+  const [isDeleteModal, setIsDeleteModal] = useState(false)
+
+  function handleClose(){
+      setIsDeleteModal(false)
+  }
+
   return (
       <Box onClick={handleClick} className="userBox" display="flex" alignItems="center" justifyContent="space-between" p={2}>
+        
+        <ConfirmDeleteModal open={isDeleteModal} handleClose={handleClose} handleDelete={handleDelete} name={userObj.firstName + " " + userObj.lastName}/>
+
         <Box display="flex" alignItems="center">
           <Avatar src={userObj.image} alt={`${userObj.firstName} ${userObj.lastName}'s avatar`} />
           <Box ml={2}>
@@ -36,7 +51,7 @@ function UserBox({ userData, setExpandedUser, setIsEditing, processDelete, index
           <IconButton aria-label="edit" onClick={handleEdit}>
             <Edit />
           </IconButton>
-          <IconButton aria-label="delete" onClick={handleDelete}>
+          <IconButton aria-label="delete" onClick={handleClickDelete}>
             <Delete />
           </IconButton>
         </Box>
