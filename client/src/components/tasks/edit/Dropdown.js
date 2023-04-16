d156dfcd64c219dc525df3319c5e8c84ce6d6677
Benjamin Rittenhouse <benjamin.rittenhouse@temple.edu>
2023-04-16 14:51:06 -0400

    dirty flag working i believe (insert)

--- client/src/components/tasks/edit/Dropdown.js
+++ client/src/components/tasks/edit/Dropdown.js
@@ -1,24 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import { Alert, AlertTitle, listItemSecondaryActionClasses } from "@mui/material";
 
-function Dropdown({assignedWebUserID, dropdownName, listItems, handleClick, handleChange, recent, selectedValue, handleSelect, handleMore, numItems, dirtyFlag}) {
+function Dropdown({ assignedWebUserID, dropdownName, listItems, handleClick, handleChange, recent, selectedValue, handleSelect, handleMore, numItems, dirtyFlag }) {
 
-    
+
     useEffect(() => {
         // handleWebUser();
         handleClick();
-      }, []);
+    }, []);
 
     return (
         <div className="dropdown">
-                <div className="searchRow">
+            <div className="searchRow">
                 <input type="text" className="nameInput" name="inputVal" placeholder="Type starting chars and click search..." value={selectedValue}
                     onChange={handleChange}
                 />
 
                 {(assignedWebUserID === null || assignedWebUserID === "") ? (
-                    <Alert severity="error" className="alert" style={{position: 'absolute', top: '1.5rem', left: '1rem'}}>
-                       Changes made. Select a user before saving. Users: {listItems.length}, Selected: {selectedValue}
+                    <Alert severity="error" className="alert" style={{ position: 'absolute', top: '1.5rem', left: '1rem' }}>
+                        Changes made. Select a user before saving. Users: {listItems.length}, Selected: {selectedValue}
                     </Alert>
                 ) : (
                     <></>
@@ -26,37 +26,37 @@ function Dropdown({assignedWebUserID, dropdownName, listItems, handleClick, hand
 
                 <button onClick={handleClick}>Search</button>
 
-                </div>
+            </div>
 
-                <div className="namesDiv">
+            <div className="namesDiv">
                 {/* list of names below input field */}
-                
-                    {listItems.length > 0 ? (
-                        <ul className="names">
-                            {listItems.map((ele) => (
-                                <option
-                                    value={ele.web_user_id}
-                                    onClick={() => handleSelect(ele.web_user_id, ele.last_name + ", " + ele.first_name)}
-                                >
-                                    {ele.last_name + ", " + ele.first_name}
-                                    
-                                </option>
-                                
-                            ))}
-                            {listItems.length === 10 && numItems > 10 ? (<option onClick={()=>handleMore(selectedValue, recent)}>More...</option>) : (null)}
-                        </ul>
-                    ) : (
-                        <>
+
+                {listItems.length > 0 ? (
+                    <ul className="names">
+                        {listItems.map((ele) => (
+                            <option
+                                value={ele.web_user_id}
+                                onClick={() => handleSelect(ele.web_user_id, ele.last_name + ", " + ele.first_name)}
+                            >
+                                {ele.last_name + ", " + ele.first_name}
+
+                            </option>
+
+                        ))}
+                        {listItems.length === 10 && numItems > 10 ? (<option onClick={() => handleMore(selectedValue, recent)}>More...</option>) : (null)}
+                    </ul>
+                ) : (
+                    <>
                         {dirtyFlag ? (
-                            null
-                        ) : (
                             <ul className="names">
                                 <option >No such user</option>
                             </ul>
+                        ) : (
+                            null
                         )}
-                        </>
-                    )}
-                    </div>
+                    </>
+                )}
+            </div>
         </div>
     )
 }
